9ab36d9c44699710e7a1038105c3ebd40ba78026
Usama Ikram <usama.ikram007@yahoo.com>
2025-11-27 14:09:28 +0900

    CLIP Analysis and Testing
    The testing and analysis is done using the weights provided by the open ai clip repository.

---
+++ dataset_helpers.py
@@ -0,0 +1,69 @@
+# This list provides the 102 class names in the correct order for the labels 0-101.
+FLOWERS102_CLASS_NAMES = [
+    "pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold", 
+    "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle", 
+    "snapdragon", "colt's foot", "king protea", "spear thistle", "yellow iris", 
+    "globe-flower", "purple coneflower", "peruvian lily", "balloon flower", "giant white arum lily", 
+    "fire lily", "pincushion flower", "fritillary", "red ginger", "grape hyacinth", 
+    "corn poppy", "prince of wales feathers", "stemless gentian", "artichoke", "sweet william", 
+    "carnation", "garden phlox", "love in a mist", "mexican aster", "alpine sea holly", 
+    "ruby-lipped cattleya", "cape flower", "great masterwort", "siam tulip", "lenten rose", 
+    "barbeton daisy", "daffodil", "sword lily", "poinsettia", "bolero deep blue", 
+    "wallflower", "marigold", "buttercup", "oxeye daisy", "common dandelion", 
+    "petunia", "wild pansy", "primula", "sunflower", "pelargonium", 
+    "himalayan blue poppy", "clematis", "hibiscus", "columbine", "desert-rose", 
+    "tree mallow", "magnolia", "cyclamen", "watercress", "canna lily", 
+    "hippeastrum", "bee balm", "ball moss", "foxglove", "bougainvillea", 
+    "camellia", "mallow", "mexican petunia", "bromelia", "blanket flower", 
+    "trumpet creeper", "blackberry lily", "common tulip", "wild rose", "bearded iris", 
+    "windflower", "tree poppy", "gazania", "azalea", "california poppy", 
+    "osteospermum", "spring crocus", "bearded iris", "common dagga", "silverbush", 
+    "wild strawberry", "morning glory", "orange dahlia", "pink-yellow dahlia", "cautleya spicata", 
+    "japanese anemone", "black-eyed susan", "silene acaulis", "common spotted orchid", "frangipani", 
+    "anthurium", "water lily", "lemon verbena", "lotus", "passion flower"
+]
+
+GENERAL_TEMPLATES = [
+    'a photo of a {}.',
+    'a bad photo of a {}.',
+    'a photo of many {}.',
+    'a sculpture of a {}.',
+    'a photo of the hard to see {}.',
+    'a low resolution photo of the {}.',
+    'a rendering of a {}.',
+    'graffiti of a {}.',
+    'a bad photo of the {}.',
+    'a cropped photo of the {}.',
+    'a tattoo of a {}.',
+    'the embroidered {}.',
+    'a photo of a cool {}.',
+    'a photo of a small {}.',
+    'a photo of a large {}.',
+]
+
+DTD_TEMPLATES = [
+    'a photo of a {} texture.',
+    'a photo of a {} pattern.',
+    'a photo of a {} thing.',
+    'a photo of a {} object.',
+    'a photo of the {} texture.',
+    'a photo of the {} pattern.',
+    'a photo of the {} thing.',
+    'a photo of the {} object.',
+]
+
+EUROSAT_TEMPLATES = [
+    'a satellite photo of {}.',
+    'a satellite photo of a {}.',
+    'a satellite photo of the {}.',
+]
+
+FGVC_TEMPLATES = [
+    'a photo of a {}, a type of aircraft.',
+    'a photo of the {}, a type of aircraft.',
+]
+
+MNIST_TEMPLATES = [
+    'a photo of the digit {}.',
+    'a photo of the number {}.',
+]
